bd3b501a725d8f508ca29188e594fecfdd161945
Marc Gomis <70306058+marcgomis17@users.noreply.github.com>
2022-08-27 01:02:24 +0000

    Product list component

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Produit } from 'src/app/models/produit';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
+})
+export class ProductListComponent implements OnInit {
+  @Input() produits: Produit[];
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
